aae7be89bebd63e4a6c6dd37ea9b1444b3ea2a6f
shortlj <eaw0224@korea.ac.kr>
2024-07-25 17:00:36 +0900

    sorter debug for ROOT6

--- bigrips_sort/afterproc_LJ.sh
+++ bigrips_sort/afterproc_LJ.sh
@@ -2,26 +2,29 @@
 set -e
 set -x
 
+#runname=idatentest
 runname=idaten
 
-InputPath=/raid/r0901/idaten24raw/vme
-OutputPath=/raid/r0901/idaten24/rootfiles
+InputPath=/home/gamma/IDATEN/2024_spring/vme
+OutputPath=/home/gamma/IDATEN/2024_spring/ROOT/vme
 
 
 makepidtree()
 {
 	jjjj=${1}
 	InputFile=${InputPath}/${runname}${jjjj}.ridf
-	#OutputFile=${OutputPath}/${runname}_${jjjj}_full.root
+	#OutputFile=${OutputPath}/${runname}${jjjj}_full.root
+	#MakeFullBigRIPSTree -i ${InputFile} -o ${OutputFile}
 	OutputFile=${OutputPath}/${runname}${jjjj}.root
 	MakePIDTree -i ${InputFile} -o ${OutputFile}
-	touch --reference=${InputFile} ${OutputFile}
+	#touch --reference=${InputFile} ${OutputFile}
+	#chgrp gamma ${OutputFile}
 }
 
 
 
-for iiii in {0089..0094}
+for iiii in {0003..0003}
 do
-	makepidtree ${iiii} &
+	makepidtree ${iiii} 
 	sleep 1
 done
